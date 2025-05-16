b7101ce32e7dc3be8da4180d663b71f3ac62c585
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-05-16 18:23:10 +0700

    UPDATE: add Retryer connect feign

--- src/main/java/be/blog_service_deploy/feign/UserServiceClient.java
+++ src/main/java/be/blog_service_deploy/feign/UserServiceClient.java
@@ -1,13 +1,23 @@
 package be.blog_service_deploy.feign;
 
 import be.blog_service_deploy.dto.UserResponse;
+import feign.Retryer;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "USER-SERVICE", url = "http://localhost:8080")
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@FeignClient(name = "USER-SERVICE", url = "http://localhost:8080/api/user")
 public interface UserServiceClient {
-    @GetMapping("/api/user/{id}")
+    @GetMapping("/{id}")
     UserResponse getUserById(@PathVariable("id") Long id);
 
+    @Bean
+    static Retryer retryer() {
+        return new Retryer.Default(100, SECONDS.toMillis(1), 3);
+    }
 }
+
+
